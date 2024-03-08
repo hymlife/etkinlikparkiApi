b0157c357f9deef761bb1325ca6b21fb3331c70d
Halil İbrahim Dursun <48247076+hymlife@users.noreply.github.com>
2024-03-08 14:28:23 +0300

    etkinlik detayi eklendi

--- app/Http/Controllers/EtkinlikController.php
+++ app/Http/Controllers/EtkinlikController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Events;
 
 class EtkinlikController extends Controller
 {
     public function getEtkinlikDetayi($id)
     {
-        $data = [
-            'message' => 'Mekan Detayi Çekilecek örneğidir.'
-        ];
+        // Belirtilen ID'ye sahip etkinliği bul
+        $etkinlik = Events::find($id);
 
-        return response()->json($data, 200);
+        // Etkinlik bulunduysa
+        if ($etkinlik) {
+            // JSON formatında etkinlik detaylarını döndür
+            return response()->json(['etkinlik' => $etkinlik], 200);
+        } else {
+            // Etkinlik bulunamadı durumunda bir hata mesajı döndür
+            $data = [
+                'message' => 'Belirtilen ID\'ye sahip etkinlik bulunamadı.'
+            ];
+            return response()->json($data, 404);
+        }
     }
 }
